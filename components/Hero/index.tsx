1db85cac7f07632b4a9de1b7be06c8aef7f4b605
Thomas Glauser <thomas.glauser01@protonmail.ch>
2024-09-08 19:36:18 +0200

    hero init

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,19 +16,46 @@ const Hero = () => {
           />
         </div>
 
-        <div className="relative z-10 flex h-full flex-col items-center justify-center space-y-4 text-center">
+        <div className="relative z-10 flex h-2/4 items-center justify-center text-center">
           <h1 className="text-5xl font-bold text-white">
             Willkommen auf der Vereinswebseite vom Gasschutzkorps!
           </h1>
+        </div>
 
-          {/* Text content below the title */}
-          <div className="max-w-2xl text-lg text-white">
+        <div className="relative z-10 w-full py-4 text-center">
+          <div className="mx-auto max-w-4xl text-lg text-white">
             <p>
               Unser Verein setzt sich aus aktiven und ehemaligen Feuerwehrleuten
               oder Personen welche mit der Feuerwehr zu tun haben/hatten
               zusammen.
             </p>
             <br></br>
+            <p>
+              Wir sind eine aktive und gesellige Gemeinschaft. Unser Vereinsjahr
+              ist gespickt mit tollen Ausflügen, gemeinsamen Aktivitäten und
+              Anlässe. Wenn wir auf unserer Vereinsreise unterwegs sind erleben
+              wir Kultur, Kulinarik und unsere Kameradschaft.
+            </p>
+            <br></br>
+            <p>
+              Du hast Lust unseren Verein kennenzulernen? Dann melde dich und
+              schau beim nächsten Anlass vorbei!
+            </p>
+            <div className=" space-x-4 p-4">
+              <Link
+                href="/contact"
+                className="inline-block rounded-md bg-red-500 px-6 py-2 font-semibold text-white transition duration-300 hover:bg-red-600"
+              >
+                <p>Kontakt</p>
+              </Link>
+              <Link
+                href="https://www.118prozent.ch/"
+                className="inline-block rounded-md bg-red-500 px-6 py-2 font-semibold text-white transition duration-300 hover:bg-red-600"
+                target="blank"
+              >
+                <p>Ich will zur Feuerwehr!</p>
+              </Link>
+            </div>
           </div>
         </div>
 
