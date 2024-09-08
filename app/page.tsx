c65c7f142adb3427375a62de80715ad8a08f110c
Thomas Glauser <thomas.glauser01@protonmail.ch>
2024-09-08 19:20:14 +0200

    layout hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,15 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
-import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
-import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
-  // other metadata
+  title: "Gasschutzkorps der Freiwilligen Feuerwehr der Stadt Zug",
+  description: "Gasschutzkorps der Freiwilligen Feuerwehr der Stadt Zug",
 };
 
 export default function Home() {
   return (
     <>
-      <ScrollUp />
       <Hero />
-      <Features />
-      <Video />
-      <Brands />
-      <AboutSectionOne />
-      <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />
-      <Blog />
-      <Contact />
     </>
   );
 }
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,10 +16,20 @@ const Hero = () => {
           />
         </div>
 
-        <div className="relative z-10 flex h-full items-center justify-center">
+        <div className="relative z-10 flex h-full flex-col items-center justify-center space-y-4 text-center">
           <h1 className="text-5xl font-bold text-white">
             Willkommen auf der Vereinswebseite vom Gasschutzkorps!
           </h1>
+
+          {/* Text content below the title */}
+          <div className="max-w-2xl text-lg text-white">
+            <p>
+              Unser Verein setzt sich aus aktiven und ehemaligen Feuerwehrleuten
+              oder Personen welche mit der Feuerwehr zu tun haben/hatten
+              zusammen.
+            </p>
+            <br></br>
+          </div>
         </div>
 
         <div className="absolute inset-0 z-0 bg-black opacity-60"></div>
