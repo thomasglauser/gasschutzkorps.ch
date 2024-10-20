d37aeafc536c026e5040fdf95ee4014ce3127b3b
Thomas Glauser <thomas.glauser01@protonmail.ch>
2024-10-20 12:45:45 +0200

    veteranen init

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,4 +1,3 @@
-import Breadcrumb from "@/components/Common/Breadcrumb";
 import Contact from "@/components/Contact";
 
 import { Metadata } from "next";
@@ -11,11 +10,6 @@ export const metadata: Metadata = {
 const ContactPage = () => {
   return (
     <>
-      <Breadcrumb
-        pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
-      />
-
       <Contact />
     </>
   );
