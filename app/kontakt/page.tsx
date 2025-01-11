ca8dc6e6c6e61b8fbd52d226b48c3a49e339f00e
Thomas Glauser <thomas.glauser01@protonmail.ch>
2025-01-11 10:30:14 +0100

    Form handling via web3forms implemented

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -10,7 +10,20 @@ export const metadata: Metadata = {
 const ContactPage = () => {
     return (
         <>
-            <Contact />
+            <section className="pb-[120px] pt-[150px]">
+                <div className="container">
+                    <div className="-mx-4 flex flex-wrap justify-center">
+                        <div className="w-full px-4 lg:w-8/12">
+                            <div>
+                                <h2 className="mb-8 text-3xl font-bold leading-tight text-white sm:text-4xl sm:leading-tight">
+                                    Kontakt
+                                </h2>
+                                <Contact />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,25 +1,33 @@
+'use client';
+
+async function handleSubmit(e: any) {
+    e.preventDefault();
+    const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            access_key: 'a18691eb-bab2-4dbe-8dd5-7c5b12a34e0c',
+            name: e.target.name.value,
+            email: e.target.email.value,
+            message: e.target.message.value,
+        }),
+    });
+    const result = await response.json();
+    if (result.success) {
+    }
+}
+
 const Contact = () => {
     return (
-        <section
-            id="contact"
-            className="overflow-hidden py-16 md:py-20 lg:py-28"
-        >
-            <div className="container">
-                <div className="-mx-4 flex flex-wrap">
-                    <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
-                        <div
-                            className="wow fadeInUp mb-12 rounded-sm  px-8 py-11 shadow-three bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
-                            data-wow-delay=".15s
-              "
-                        >
-                            <h2 className="mb-3 text-2xl font-bold text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                                Need Help? Open a Ticket
-                            </h2>
-                            <p className="mb-12 text-base font-medium text-body-color">
-                                Our support team will get back to you ASAP via
-                                email.
-                            </p>
-                            <form>
+        <section id="contact" className="overflow-hidden">
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <div className="wow fadeInUp mb-12 rounded-sm  px-8 py-11 shadow-three bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]">
+                            <form onSubmit={handleSubmit}>
                                 <div className="-mx-4 flex flex-wrap">
                                     <div className="w-full px-4 md:w-1/2">
                                         <div className="mb-8">
@@ -27,11 +35,13 @@ const Contact = () => {
                                                 htmlFor="name"
                                                 className="mb-3 block text-sm font-medium text-white"
                                             >
-                                                Your Name
+                                                Name
                                             </label>
                                             <input
                                                 type="text"
-                                                placeholder="Enter your name"
+                                                name="name"
+                                                required
+                                                placeholder="Wie lautet dein Vor/Nachname?"
                                                 className="w-full rounded-sm border border-stroke  px-6 py-3 text-base outline-none border-transparent bg-[#2C303B] text-body-color-dark shadow-two focus:border-primary focus:shadow-none"
                                             />
                                         </div>
@@ -42,11 +52,13 @@ const Contact = () => {
                                                 htmlFor="email"
                                                 className="mb-3 block text-sm font-medium text-white"
                                             >
-                                                Your Email
+                                                E-Mail
                                             </label>
                                             <input
                                                 type="email"
-                                                placeholder="Enter your email"
+                                                name="email"
+                                                required
+                                                placeholder="Wie lautet deine E-Mail?"
                                                 className="w-full rounded-sm border border-stroke px-6 py-3 text-base outline-none border-transparent bg-[#2C303B] text-body-color-dark shadow-two focus:border-primary focus:shadow-none"
                                             />
                                         </div>
@@ -57,19 +69,20 @@ const Contact = () => {
                                                 htmlFor="message"
                                                 className="mb-3 block text-sm font-medium text-white"
                                             >
-                                                Your Message
+                                                Nachricht
                                             </label>
                                             <textarea
                                                 name="message"
+                                                required
                                                 rows={5}
-                                                placeholder="Enter your Message"
+                                                placeholder="Wie lautet deine Nachricht?"
                                                 className="w-full resize-none rounded-sm border border-stroke px-6 py-3 text-base outline-none border-transparent bg-[#2C303B] text-body-color-dark shadow-two focus:border-primary focus:shadow-none"
                                             ></textarea>
                                         </div>
                                     </div>
                                     <div className="w-full px-4">
                                         <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90 shadow-submit-dark">
-                                            Submit Ticket
+                                            Senden
                                         </button>
                                     </div>
                                 </div>
