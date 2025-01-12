ac64f3a1061f76a43223a82d4d237668397a1da5
Thomas Glauser <thomas.glauser01@protonmail.ch>
2025-01-12 23:03:00 +0100

    mobile friendly init

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -6,8 +6,8 @@ import heroImage from '/public/images/general/hero.webp';
 const Hero = () => {
     return (
         <>
-            <div className="relative h-screen w-full">
-                <div className="absolute inset-0">
+            <div className="relative min-h-screen w-full">
+                <div className="absolute inset-0 h-screen">
                     <Image
                         src={heroImage}
                         alt="Background"
@@ -18,19 +18,17 @@ const Hero = () => {
                     />
                 </div>
 
-                <div className="relative z-10 flex h-2/4 items-center justify-center text-center">
-                    <h1 className="text-5xl font-bold text-white">
+                <div className="relative z-10 flex flex-col items-center justify-start pt-32 sm:pt-40 md:pt-48 lg:pt-56 xl:pt-64 text-center">
+                    <h1 className="text-xl sm:text-2xl md:text-4xl font-bold text-white px-4">
                         Willkommen auf der Vereinswebseite vom Gasschutzkorps!
                     </h1>
-                </div>
 
-                <div className="relative z-10 w-full py-4 text-center">
-                    <div className="mx-auto max-w-4xl text-lg text-white">
+                    <div className="relative w-full py-6 px-4 text-white text-sm sm:text-base md:text-lg">
                         <p>
                             Unser Verein setzt sich aus aktiven und ehemaligen
                             Feuerwehrleuten zusammen.
                         </p>
-                        <br></br>
+                        <br />
                         <p>
                             Wir sind eine aktive und gesellige Gemeinschaft.
                             Unser Vereinsjahr ist gespickt mit tollen Ausflügen,
@@ -38,12 +36,12 @@ const Hero = () => {
                             unserer Vereinsreise unterwegs sind, erleben wir
                             Kultur, Kulinarik und unsere Kameradschaft.
                         </p>
-                        <br></br>
+                        <br />
                         <p>
                             Du hast Lust, unseren Verein kennenzulernen? Dann
                             melde dich und schau beim nächsten Anlass vorbei!
                         </p>
-                        <div className=" space-x-4 p-4">
+                        <div className="flex flex-wrap justify-center space-x-4 mt-4">
                             <Link
                                 href="/kontakt"
                                 className="inline-block rounded-md bg-red-500 px-6 py-2 font-semibold text-white transition duration-300 hover:bg-red-600"
@@ -53,7 +51,7 @@ const Hero = () => {
                             <Link
                                 href="https://www.118prozent.ch/"
                                 className="inline-block rounded-md bg-red-500 px-6 py-2 font-semibold text-white transition duration-300 hover:bg-red-600"
-                                target="blank"
+                                target="_blank"
                             >
                                 Ich will zur Feuerwehr!
                             </Link>
